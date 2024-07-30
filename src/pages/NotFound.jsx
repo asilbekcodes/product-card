b37864e6b452fa484958cec6ca47f20598013feb
Asilbek Codes <asilbeksuvonov43@gmail.com>
2024-07-30 20:24:36 +0500

    new pages

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='container px-32 mx-auto'>
+      <img className='w-[100%]' src="https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Error-Page.gif" alt="" />
+    </div>
+  )
+}
+
+export default NotFound
