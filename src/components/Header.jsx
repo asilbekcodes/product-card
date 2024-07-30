d21c48cd3fa206d8de86cf26fdb7cd4e95b99023
Asilbek Codes <asilbeksuvonov43@gmail.com>
2024-07-30 20:25:20 +0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = ({ length }) => {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Product</span>
           </a>
           <button
             data-collapse-toggle="navbar-default"
